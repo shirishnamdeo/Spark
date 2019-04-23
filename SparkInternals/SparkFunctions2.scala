f114be745bd59daf34d373b42b274a96f56b57b0
Shirish Namdeo <shirishnamdeo.iitb@gmail.com>
2019-04-23 17:33:06 +0530

    Update

--- SparkInternals/SparkFunctions2.scala
+++ SparkInternals/SparkFunctions2.scala
@@ -8,25 +8,25 @@ ________________________________________________________________________________
 // Creating Methods and Converting them to Function Objects
 
 def count_null(dataFrame: DataFrame, col:String): String = {
-	dataFrame.select(col).filter(dataFrame(col).isNull).count.toString
+    dataFrame.select(col).filter(dataFrame(col).isNull).count.toString
 }
 val func_count_null: (DataFrame, String) => String = count_null
 
 
 def count_nan(dataFrame: DataFrame, col:String): String = {
-	dataFrame.select(col).filter(dataFrame(col).isNaN).count.toString
+    dataFrame.select(col).filter(dataFrame(col).isNaN).count.toString
 }
 val func_count_nan: (DataFrame, String) => String = count_nan
 
 
 def count_empty_string(dataFrame: DataFrame, col:String): String = {
-	dataFrame.select(col).filter(dataFrame(col) === "").count.toString
+    dataFrame.select(col).filter(dataFrame(col) === "").count.toString
 }
 val func_count_empty_string: (DataFrame, String) => String = count_empty_string
 
 
 def count_single_space(dataFrame: DataFrame, col:String): String = {
-	dataFrame.select(col).filter(dataFrame(col) === " ").count.toString
+    dataFrame.select(col).filter(dataFrame(col) === " ").count.toString
 }
 val func_count_single_space: (DataFrame, String) => String = count_single_space
 
@@ -39,17 +39,17 @@ ________________________________________________________________________________
 //GenericFunction
 
 def genericFunction(dataFrame: DataFrame, functionArray:Array[((DataFrame, String) => String, String, String)], columnList:Array[String]) = {
-	val function_objects = functionArray.map(func => func._1)
-	val function_names = functionArray.map(func => func._2)
-	val function_dtypes = functionArray.map(func => func._3)
-	val result_df_colNames = Seq("column_names") ++ function_names.toSeq
-	val result_df_colTypes = Seq("string") ++ function_dtypes.toSeq
-	val result_array = columnList.map(col => Array(col) ++ function_objects.map(func => func(dataFrame, col) ))
-	val selectExprs = 0 until (functionArray.size + 1) map(i => $"temp".getItem(i).as(s"col$i"))
-	val result_df = result_array.toSeq.toDF.withColumnRenamed("value” , “temp").select(selectExprs:_*).toDF(result_df_colNames: _*)
-	val result_df2 = result_df.select((result_df_colNames zip result_df_colTypes).toSeq.map{case (column_name, 
-	column_type) => col(column_name).cast(column_type)}: _*)
-	result_df2
+    val function_objects = functionArray.map(func => func._1)
+    val function_names = functionArray.map(func => func._2)
+    val function_dtypes = functionArray.map(func => func._3)
+    val result_df_colNames = Seq("column_names") ++ function_names.toSeq
+    val result_df_colTypes = Seq("string") ++ function_dtypes.toSeq
+    val result_array = columnList.map(col => Array(col) ++ function_objects.map(func => func(dataFrame, col) ))
+    val selectExprs = 0 until (functionArray.size + 1) map(i => $"temp".getItem(i).as(s"col$i"))
+    val result_df = result_array.toSeq.toDF.withColumnRenamed("value” , “temp").select(selectExprs:_*).toDF(result_df_colNames: _*)
+    val result_df2 = result_df.select((result_df_colNames zip result_df_colTypes).toSeq.map{case (column_name, 
+    column_type) => col(column_name).cast(column_type)}: _*)
+    result_df2
 }
 
 
@@ -78,11 +78,11 @@ ________________________________________________________________________________
 
 
 val data = Seq(
-	(1, "col2_val1",  "col4_val1", 50),
-	(2, "col2_val2",  "col4_val1", 12),
-	(3, "col2_val3",  " ",         11),
-	(4, null,         "col4_val1",  0),
-	(5, "",           "col4_val1", 55)
+    (1, "col2_val1",  "col4_val1", 50),
+    (2, "col2_val2",  "col4_val1", 12),
+    (3, "col2_val3",  " ",         11),
+    (4, null,         "col4_val1",  0),
+    (5, "",           "col4_val1", 55)
 )
 data: Seq[(Int, String, String, Int)] = List((1,col2_val1,col4_val1,50), (2,col2_val2,col4_val1,12), (3,col2_val3," ",11), (4,null,col4_val1,0), (5,"",col4_val1,55))
 
@@ -135,15 +135,15 @@ ________________________________________________________________________________
 
 
 val data = Seq(
-	(1, "col2_val1", null, "col4_val1", 50),
-	(2, "col2_val2", 4,    "col4_val1", 12),
-	(3, "col2_val3", " ",  "col4_val1", null),
-	(4, null,        1234, "col4_val1", Double.NaN),
-	(5, "",          null, "col4_val1", 55)
+    (1, "col2_val1", null, "col4_val1", 50),
+    (2, "col2_val2", 4,    "col4_val1", 12),
+    (3, "col2_val3", " ",  "col4_val1", null),
+    (4, null,        1234, "col4_val1", Double.NaN),
+    (5, "",          null, "col4_val1", 55)
 )
 // data: Seq[(Int, String, Any, String, Any)] = 
-// 	List((1,col2_val1,null,col4_val1,50), (2,col2_val2,4,col4_val1,12), (3,col2_val3," ",col4_val1,null), (4,null,1234,col4_val1,NaN), 
-// 		(5,"",null,col4_val1,55))
+//  List((1,col2_val1,null,col4_val1,50), (2,col2_val2,4,col4_val1,12), (3,col2_val3," ",col4_val1,null), (4,null,1234,col4_val1,NaN), 
+//      (5,"",null,col4_val1,55))
 
 
 val dataFrame = data.toDF("colName1", "colName2", "colName3", "colName4", "colName5")
