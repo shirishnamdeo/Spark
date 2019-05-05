70ed74772279ceb7b0e0e7774eb91451bf8a3cef
Shirish Namdeo <shirishnamdeo.iitb@gmail.com>
2019-05-06 01:41:19 +0530

    Update

--- SparkInternals/SparkFunctions4.scala
+++ SparkInternals/SparkFunctions4.scala
@@ -3,8 +3,8 @@
 // Method to take a dataFrame and Array of String with column names to be excluded, and return the dataFrame with excluded columns.
 
 def Method_ExcludeColumnsFromDF(dataFrame: DataFrame, excluded_column_list: Array[String]) = {
-	val included_column_list = dataFrame.columns.filter( column_name => !excluded_column_list.contains(column_name))
-	dataFrame.select(included_column_list.head, included_column_list.tail: _ *)
+    val included_column_list = dataFrame.columns.filter( column_name => !excluded_column_list.contains(column_name))
+    dataFrame.select(included_column_list.head, included_column_list.tail: _ *)
 }
 
  
@@ -19,9 +19,9 @@ def Method_ExcludeColumnsFromDF(dataFrame: DataFrame, excluded_column_list: Arra
 // Takes a Prediction dataFrame, extract the labels and prediction columns from it, and return the PAIR rdd with the two values as a pair.
 
 def Method_LabelPrediction_RDD(prediction_df: DataFrame) = {
-	val label_prediction_rdd = prediction_df.select("label", "prediction").rdd
-	val prediction_label_rdd = label_prediction_rdd.map(x => (x(1).toString.toDouble, x(0).toString.toDouble))
-	prediction_label_rdd
+    val label_prediction_rdd = prediction_df.select("label", "prediction").rdd
+    val prediction_label_rdd = label_prediction_rdd.map(x => (x(1).toString.toDouble, x(0).toString.toDouble))
+    prediction_label_rdd
 }
 
 // __________________________________________________________________________________________________________________________________________________
@@ -36,10 +36,10 @@ import org.apache.spark.sql.functions._
 import org. apache. spark.sql.expressions.Window
 
 def Method_GrpCount(dataFrame: DataFrame, column_name: String) = {
-	val count_column = "count_" + column_name
-	val countDF = dataFrame.select(col(column_name)).groupBy(column_name).agg(count(column_name).alias(count_column)).withColumn("count_percentage", format_number(col(count_column) / dataFrame.count, 8)).orderBy(desc(count_column))
-	val countDF_cummulative = countDF.withColumn("cummulative_sum", sum(count_column).over(Window.orderBy(desc(count_column)))).withColumn("cumm_percentage", format_number($"cummulative_sum" / dataFrame.count, 8))
-	countDF_cummulative
+    val count_column = "count_" + column_name
+    val countDF = dataFrame.select(col(column_name)).groupBy(column_name).agg(count(column_name).alias(count_column)).withColumn("count_percentage", format_number(col(count_column) / dataFrame.count, 8)).orderBy(desc(count_column))
+    val countDF_cummulative = countDF.withColumn("cummulative_sum", sum(count_column).over(Window.orderBy(desc(count_column)))).withColumn("cumm_percentage", format_number($"cummulative_sum" / dataFrame.count, 8))
+    countDF_cummulative
 }
 
 
@@ -47,8 +47,8 @@ def Method_GrpCount(dataFrame: DataFrame, column_name: String) = {
 
 // Below DistinctCount function will just count the distinct values in each group. Method GrpCount is more informative.
 def Method_DistinctCount(dataFrame: DataFrame, column_name: String) = {
-	val column_name_count = column_name + "_count"
-	dataFrame.select(col(column_name)).groupBy(column_name).agg(count(column_name).alias(column_name_count)).orderBy(desc(column_name_count))
+    val column_name_count = column_name + "_count"
+    dataFrame.select(col(column_name)).groupBy(column_name).agg(count(column_name).alias(column_name_count)).orderBy(desc(column_name_count))
 }
 
 // Method_GrpCount(dataFrame3, "<column_name>").show(50)
@@ -61,21 +61,21 @@ def Method_DistinctCount(dataFrame: DataFrame, column_name: String) = {
 
 // Efficient Code
 def Method_Crosstab_With_Count(dataFrame: DataFrame, left_column_name: String, upper_column_name: String) = {
-	val crosstabDF = dataFrame.stat.crosstab(1eft_column_name, upper_column_name)
-	//
-	// Excluding the first column name, and incuding all the column representing the values of a upper_column_name's column.
-	val columnsToSum = crosstabDF.columns.filter(!_.contains(crosstabDF.columns.head))
-	val crosstabDF_with_HorizontalSum = crosstabDF.withColumn("horizontalSum", columnsToSum.map(x => col(x)).reduce((c1, c2) => cl + c2)).orderBy(desc("horizontalSum"))
-	crosstabDF_with_HorizontalSum
+    val crosstabDF = dataFrame.stat.crosstab(left_column_name, upper_column_name)
+    //
+    // Excluding the first column name, and incuding all the column representing the values of a upper_column_name's column.
+    val columnsToSum = crosstabDF.columns.filter(!_.contains(crosstabDF.columns.head))
+    val crosstabDF_with_HorizontalSum = crosstabDF.withColumn("horizontalSum", columnsToSum.map(x => col(x)).reduce((c1, c2) => c1 + c2)).orderBy(desc("horizontalSum"))
+    crosstabDF_with_HorizontalSum
 }
 
  
 // Uneffieffieient Method involving join. 
 // def Method_Crosstab_With_Count(dataFrame: DataFrame, left_column_name: String, upper_column_name: String) = {
-// 	val crosstabDF = dataFrame.stat.crosstab(left_column_name, upper_column_name)
-// 	val countDF = dataFrame.select(col(1eft_column_name)).groupBy(1eft_column_name).agg(count(left_column_name))
-// 	val joinedDF = crosstabDF.join(countDF, col("human_tax_cat_business_unit"), col("human_tex_cat"), "inner").orderBy(desc("count(human_tax_cat)"))
-// 	joinedDF
+//  val crosstabDF = dataFrame.stat.crosstab(left_column_name, upper_column_name)
+//  val countDF = dataFrame.select(col(1eft_column_name)).groupBy(1eft_column_name).agg(count(left_column_name))
+//  val joinedDF = crosstabDF.join(countDF, col("human_tax_cat_business_unit"), col("human_tax_cat"), "inner").orderBy(desc("count(human_tax_cat)"))
+//  joinedDF
 // }
 
 
